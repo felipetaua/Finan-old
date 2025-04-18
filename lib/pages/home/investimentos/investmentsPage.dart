c701cf8f791e0d2f219081fa85d35d688335c737
felipetaua <taua.felipee@gmail.com>
2025-04-18 12:32:37 -0300

    adicionando imagens aleatorias para os ativos

--- lib/pages/home/investimentos/investmentsPage.dart
+++ lib/pages/home/investimentos/investmentsPage.dart
@@ -13,70 +13,70 @@ const listaAcoes = [
     'preco': 'R\$29,45',
     'variacao': '+1.5%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'VALE3',
     'preco': 'R\$68,10',
     'variacao': '-0.3%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'ITUB4',
     'preco': 'R\$28,70',
     'variacao': '+0.9%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'BBDC4',
     'preco': 'R\$23,50',
     'variacao': '-1.2%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'ABEV3',
     'preco': 'R\$15,30',
     'variacao': '+0.5%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'BBAS3',
     'preco': 'R\$38,20',
     'variacao': '+0.8%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'WEGE3',
     'preco': 'R\$42,00',
     'variacao': '+2.1%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'MGLU3',
     'preco': 'R\$4,85',
     'variacao': '-2.5%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'LREN3',
     'preco': 'R\$36,10',
     'variacao': '+1.3%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'SUZB3',
     'preco': 'R\$56,40',
     'variacao': '-0.6%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
 ];
 
@@ -86,70 +86,70 @@ const listaFundos = [
     'preco': 'R\$88,60',
     'variacao': '+2.1%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'XPLG11',
     'preco': 'R\$112,45',
     'variacao': '-0.8%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'HGLG11',
     'preco': 'R\$170,90',
     'variacao': '+0.7%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'KNRI11',
     'preco': 'R\$143,10',
     'variacao': '-0.4%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'MXRF11',
     'preco': 'R\$10,90',
     'variacao': '+1.2%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'VISC11',
     'preco': 'R\$104,00',
     'variacao': '+1.0%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'HGRE11',
     'preco': 'R\$129,60',
     'variacao': '-1.0%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'RECR11',
     'preco': 'R\$95,70',
     'variacao': '+0.3%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'FIIB11',
     'preco': 'R\$140,80',
     'variacao': '-0.2%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'JSRE11',
     'preco': 'R\$110,00',
     'variacao': '+1.4%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
 ];
 
@@ -159,70 +159,70 @@ const listaCriptos = [
     'preco': 'R\$300.000,00',
     'variacao': '+3.2%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Ethereum',
     'preco': 'R\$20.000,00',
     'variacao': '+1.0%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Litecoin',
     'preco': 'R\$600,00',
     'variacao': '+0.8%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Ripple',
     'preco': 'R\$3,50',
     'variacao': '-2.0%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Cardano',
     'preco': 'R\$1,30',
     'variacao': '+4.1%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Polkadot',
     'preco': 'R\$38,50',
     'variacao': '+0.9%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Solana',
     'preco': 'R\$180,00',
     'variacao': '-1.3%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Chainlink',
     'preco': 'R\$60,00',
     'variacao': '+2.7%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Dogecoin',
     'preco': 'R\$0,90',
     'variacao': '+1.1%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'Shiba Inu',
     'preco': 'R\$0,00007',
     'variacao': '-0.5%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
 ];
 
@@ -232,70 +232,70 @@ const listaEtfs = [
     'preco': 'R\$300,00',
     'variacao': '+1.7%',
     'up': true,
-    'icon': 'assets/images/investimento/',
+    'icon': 'assets/images/investimento/abstratos/',
   },
   {
     'nome': 'BOVA11',
     'preco': 'R\$110,00',
     'variacao': '-0.5%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'SMAL11',
     'preco': 'R\$115,00',
     'variacao': '+2.3%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'DIVO11',
     'preco': 'R\$110,20',
     'variacao': '+0.4%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'XFIX11',
     'preco': 'R\$100,00',
     'variacao': '-1.1%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'ECOO11',
     'preco': 'R\$92,40',
     'variacao': '+1.5%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'FIND11',
     'preco': 'R\$87,10',
     'variacao': '-0.3%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'HASH11',
     'preco': 'R\$48,90',
     'variacao': '+3.8%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'BOVX11',
     'preco': 'R\$120,00',
     'variacao': '-1.0%',
     'up': false,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
   {
     'nome': 'GOVE11',
     'preco': 'R\$89,30',
     'variacao': '+2.6%',
     'up': true,
-    'icon': 'assets/images/investimentos/',
+    'icon': 'assets/images/investimentos/abstratos/',
   },
 ];
 
@@ -307,15 +307,82 @@ List<Map<String, dynamic>> todosAtivos = [
 ];
 
 List<String> imagens = [
-  'https://exemplo.com/imagem1.jpg',
-  'https://exemplo.com/imagem2.jpg',
-  'https://exemplo.com/imagem3.jpg',
+  'invest (1).png',
+  'invest (2).png',
+  'invest (3).png',
+  'invest (4).png',
+  'invest (5).png',
+  'invest (6).png',
+  'invest (7).png',
+  'invest (8).png',
+  'invest (9).png',
+  'invest (10).png',
+  'invest (11).png',
+  'invest (12).png',
+  'invest (13).png',
+  'invest (14).png',
+  'invest (15).png',
+  'invest (16).png',
+  'invest (17).png',
+  'invest (18).png',
+  'invest (19).png',
+  'invest (20).png',
+  'invest (21).png',
+  'invest (22).png',
+  'invest (23).png',
+  'invest (24).png',
+  'invest (25).png',
+  'invest (26).png',
+  'invest (27).png',
+  'invest (28).png',
+  'invest (29).png',
+  'invest (30).png',
+  'invest (31).png',
+  'invest (32).png',
+  'invest (33).png',
+  'invest (34).png',
+  'invest (35).png',
+  'invest (36).png',
+  'invest (37).png',
+  'invest (38).png',
+  'invest (39).png',
+  'invest (40).png',
+  'invest (41).png',
+  'invest (42).png',
+  'invest (43).png',
+  'invest (44).png',
+  'invest (45).png',
+  'invest (46).png',
+  'invest (47).png',
+  'invest (48).png',
+  'invest (49).png',
+  'invest (50).png',
+  'invest (51).png',
+  'invest (52).png',
+  'invest (53).png',
+  'invest (54).png',
+  'invest (55).png',
+  'invest (56).png',
+  'invest (57).png',
+  'invest (58).png',
+  'invest (59).png',
+  'invest (60).png',
+  'invest (61).png',
+  'invest (62).png',
+  'invest (63).png',
+  'invest (64).png',
+  'invest (65).png',
+  'invest (66).png',
+  'invest (67).png',
+  'invest (68).png',
+  'invest (69).png',
 ];
 
 String getRandomAssetImage(List<String> nomesArquivos) {
   final random = Random();
   final nomeEscolhido = nomesArquivos[random.nextInt(nomesArquivos.length)];
-  return 'assets/images/investimentos/$nomeEscolhido';
+  final caminho = 'assets/images/investimentos/abstratos/$nomeEscolhido';
+  return caminho;
 }
 
 class InvestimentosPage extends StatefulWidget {
@@ -364,7 +431,12 @@ class _InvestimentosPageState extends State<InvestimentosPage> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => ListaDetalhadaPage(titulo: titulo, ativos: lista),
+        builder:
+            (context) => ListaDetalhadaPage(
+              titulo: titulo,
+              ativos: lista,
+              imagens: imagens, // adiciona isso
+            ),
       ),
     );
   }
@@ -407,53 +479,47 @@ class _InvestimentosPageState extends State<InvestimentosPage> {
 
               // Resultados da pesquisa
               if (_searchResults.isNotEmpty)
-                Positioned(
-                  top:
-                      80, // ajusta conforme a altura do campo de busca + padding
-                  left: 20,
-                  right: 20,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          color: Colors.black26,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    constraints: BoxConstraints(maxHeight: 400),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: _searchResults.length,
-                      itemBuilder: (context, index) {
-                        final ativo = _searchResults[index];
-                        return ListTile(
-                          leading: Image.asset(
-                            ativo['icon'] ?? '',
-                            width: 30,
-                            height: 30,
-                            errorBuilder: (context, error, stackTrace) {
-                              return const Icon(Icons.image_not_supported);
-                            },
-                          ),
-                          title: Text(ativo['nome']),
-                          subtitle: Text(ativo['preco']),
-                          trailing: Text(
-                            ativo['variacao'],
-                            style: TextStyle(
-                              color: ativo['up'] ? Colors.green : Colors.red,
-                            ),
-                          ),
-                          onTap: () {
-                            // Você pode navegar ou exibir detalhes do ativo aqui
-                            _abrirLista('Criptos', listaCriptos);
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  constraints: BoxConstraints(maxHeight: 400),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _searchResults.length,
+                    itemBuilder: (context, index) {
+                      final ativo = _searchResults[index];
+                      return ListTile(
+                        leading: Image.asset(
+                          getRandomAssetImage(imagens),
+                          width: 30,
+                          height: 30,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Icon(Icons.image_not_supported);
                           },
-                        );
-                      },
-                    ),
+                        ),
+
+                        title: Text(ativo['nome']),
+                        subtitle: Text(ativo['preco']),
+                        trailing: Text(
+                          ativo['variacao'],
+                          style: TextStyle(
+                            color: ativo['up'] ? Colors.green : Colors.red,
+                          ),
+                        ),
+                        onTap: () {
+                          _abrirLista('Resultado da busca', _searchResults);
+                        },
+                      );
+                    },
                   ),
                 ),
 
