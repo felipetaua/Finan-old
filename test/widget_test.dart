9b951037a059abbdd80b6a7f4214c66add98f886
felipetaua <taua.felipee@gmail.com>
2025-10-18 18:15:04 -0300

    remasterizando o app completamente para nova identidade visual

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:finan/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App shows initialization text', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FinanApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify our HomeScreen displays the expected text.
+    expect(find.text('Firebase initialized — app default'), findsOneWidget);
   });
 }
