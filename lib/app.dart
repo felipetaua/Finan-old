824745be6f868dae09febaa0c6b32b026fe1e673
felipetaua <taua.felipee@gmail.com>
2025-12-01 15:19:51 -0300

    adicionado novos navegações do app

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome/welcome_screen.dart';
-import 'screens/auth/login_screen.dart';
+import 'screens/auth/AuthEntryScreen.dart';
+import 'screens/auth/login_with_password_screen.dart';
 import 'screens/auth/register_screen.dart';
 import 'screens/auth/forgot_password_screen.dart';
 import 'screens/home/home_screen.dart';
@@ -23,11 +24,20 @@ class FinanApp extends StatelessWidget {
       routes: {
         '/': (context) => const WelcomeScreen(),
         '/splash': (context) => const SplashScreen(),
-        '/login': (context) => const LoginScreen(),
+        '/login': (context) => const AuthEntryScreen(),
         '/register': (context) => const RegisterScreen(),
         '/forgot-password': (context) => const ForgotPasswordScreen(),
         '/home': (context) => const HomeScreen(),
         '/terms': (context) => const TermsOfUseScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/login-password') {
+          final email = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (context) => LoginWithPasswordScreen(email: email),
+          );
+        }
+        return null;
       },
     );
   }
